e602a5b975d2c9d865d5a7e91722fc6328ff3288
IT24610827 <it24610827@my.sliit.lk>
2025-10-05 01:32:22 +0530

    Add files via upload

---
+++ Lab Sheet 09/IT24610827.r
@@ -0,0 +1,22 @@
+setwd("C:\\Users\\user\\Desktop\\IT24610827_Lab_09")
+getwd()
+
+x<-c(3,7,11,0,7,0,4,5,6,2)
+t.test(x,mu=3)
+
+weight <-c(17.6,20.6,22.2,15.3,20.9,21.0,18.9,18.9,18.2)
+t.test(weight,mu=25,alternative="less")
+
+res<-t.test(weight,mu=25,alternative="less")
+res$statistic
+res$p.value
+res$conf.int
+
+
+
+
+#exercise
+y<-rnorm(25,mean=45,sd=2)
+print(y)
+t_test_result<-t.test(y,mu=46,alternative="less")
+print(t_test_result)
